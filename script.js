afdf54516ccacdf563feb96f2f32fae4f42c6b0e
laharke <ronconimauro@yahoo.com.ar>
2021-10-05 11:16:15 -0300

    making the logic work, almost done

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
 //VALOR MENSUAL MODELO ONE
-const valorMensualCantRef = document.querySelector("#valorMensualCant");
-const valorMensualCant = document.querySelector("#valorMensualCant").value;
-const valorMensualPrice = document.querySelector("#valorMensualPrice").innerHTML;
-const valorMensualTotal = document.querySelector("#valorMensualTotal");
-let newValorMensualTotal = 0;
+const valorMensualCantRef1 = document.querySelector("#valorMensualCant1");
+const valorMensualCant1 = document.querySelector("#valorMensualCant1").value;
+let valorMensualPrice1 = document.querySelector("#valorMensualPrice1").innerHTML;
+valorMensualPrice1 = valorMensualPrice1.substring(1);
+const valorMensualTotal1 = document.querySelector("#valorMensualTotal1");
+let newValorMensualTotal1 = 0;
 
-valorMensualCantRef.addEventListener('input', () => {
-    newValorMensualTotal = valorMensualCantRef.value * valorMensualPrice;
-    valorMensualTotal.innerHTML = newValorMensualTotal;
+valorMensualCantRef1.addEventListener('input', () => {
+    newValorMensualTotal1 = valorMensualCantRef1.value * valorMensualPrice1;
+    if (newValorMensualTotal1 == "1600" || newValorMensualTotal1 == "800"){
+        valorMensualTotal1.innerHTML = "$" + 2400;
+    }
+    else{
+    valorMensualTotal1.innerHTML = "$" + newValorMensualTotal1;
+    }
 });
 
-console.log(valorMensualCant)
-console.log(valorMensualCant, valorMensualPrice, valorMensualTotal);
+
+//VALOR MENSUAL MODELO LIGHT
+const valorMensualCantRef2 = document.querySelector("#valorMensualCant2");
+const valorMensualCant2 = document.querySelector("#valorMensualCant2").value;
+let valorMensualPrice2 = document.querySelector("#valorMensualPrice2").innerHTML;
+valorMensualPrice2 = valorMensualPrice2.substring(1);
+const valorMensualTotal2 = document.querySelector("#valorMensualTotal2");
+let newValorMensualTotal2 = 0;
+
+valorMensualCantRef2.addEventListener('input', () => {
+    newValorMensualTotal2 = valorMensualCantRef2.value * valorMensualPrice2;
+    if (newValorMensualTotal2 == "900" || newValorMensualTotal2 == "1800"){
+        valorMensualTotal2.innerHTML = "$" + 2700;
+    }
+    else {
+    valorMensualTotal2.innerHTML = "$" + newValorMensualTotal2;
+    }
+});
+
+//METO EN UNA LISTA LA REFERENCIA A TODOS LOS PRECIOS, CANTIDADES Y TOTALES QUE FALTAN ,CADA UNO VA A CORRESPONDER CON SU INDICE
+const cantidadRefs = document.querySelectorAll(".valorMensualCant");
+const priceRefs = document.querySelectorAll(".valorMensualPrice");
+const priceTotal = document.querySelectorAll(".valorMensualTotal");
+
+for (let i = 0; i < 7; i++){
+    
+    cantidadRefs[i].addEventListener('input', () => {
+        let cantidad = cantidadRefs[i].value;
+        let valorMensual = priceRefs[i].innerHTML;
+        valorMensual = valorMensual.substring(1);
+        let newValorMensual = 0;
+        newValorMensual = cantidad * valorMensual;
+        priceTotal[i].innerHTML = "$" + newValorMensual;
+    });
+};
